b50db4be4dd49fdc95ed122ab357490ca6a8f698
Avinash <60576485+Maidira@users.noreply.github.com>
2023-07-20 04:03:10 +0530

    Update Danny's_Diner_Solution.sql

--- Case Study #1 - Danny's Diner/Danny's_Diner_Solution.sql
+++ Case Study #1 - Danny's Diner/Danny's_Diner_Solution.sql
@@ -1,1 +1,30 @@
+----------------------------------
+-- CASE STUDY #1: DANNY'S DINER --
+----------------------------------
 
+-- Tool used: MySQL Server
+
+-------------------------------------
+-- CASE STUDY QUESTIONS AND ANSWERS--
+-------------------------------------
+
+
+-- 1. What is the total amount each customer spent at the restaurant?
+
+SELECT 
+  s.customer_id, 
+  SUM(m.price) AS total_sales
+FROM sales s
+INNER JOIN menu m
+  ON s.product_id = m.product_id
+GROUP BY s.customer_id
+ORDER BY s.customer_id ASC;
+
+
+-- 2. How many days has each customer visited the restaurant?
+
+SELECT 
+  customer_id, 
+  COUNT(DISTINCT order_date) AS visit_count
+FROM sales
+GROUP BY customer_id;
