7dc4981153166e3cdb73a62e18be59e428019834
Avinash <60576485+Maidira@users.noreply.github.com>
2023-07-20 04:09:22 +0530

    Update Danny's_Diner_Solution.sql

--- Case Study #1 - Danny's Diner/Danny's_Diner_Solution.sql
+++ Case Study #1 - Danny's Diner/Danny's_Diner_Solution.sql
@@ -28,3 +28,34 @@ SELECT
   COUNT(DISTINCT order_date) AS visit_count
 FROM sales
 GROUP BY customer_id;
+
+
+-- 3. What was the first item from the menu purchased by each customer?
+-- Since the timestamp is missing, all items bought on the first day are considered the first item
+
+WITH ordered_sales AS (
+  SELECT 
+    s.customer_id, 
+    s.order_date, 
+    m.product_name,
+    DENSE_RANK() OVER (
+      PARTITION BY s.customer_id 
+      ORDER BY s.order_date) AS rank
+  FROM sales s
+  INNER JOIN menu m
+    ON s.product_id = m.product_id
+)
+
+SELECT 
+  customer_id, 
+  product_name
+FROM ordered_sales
+WHERE rank = 1
+GROUP BY customer_id, product_name;
+
+
+
+
+
+
+
