1f0276887f5c1a36817c809fe889fcea360b77c3
Avinash <60576485+Maidira@users.noreply.github.com>
2023-09-26 22:15:12 +0530

    Update Pizza_Metrics_Solution.sql

--- Case Study #2 - Pizza Runner/Pizza_Metrics_Solution.sql
+++ Case Study #2 - Pizza Runner/Pizza_Metrics_Solution.sql
@@ -96,3 +96,52 @@ FROM pizza_count_cte;
 
 -- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
 
+SELECT 
+  c.customer_id,
+  SUM(
+    CASE WHEN c.exclusions <> ' ' OR c.extras <> ' ' THEN 1
+    ELSE 0
+    END) AS at_least_1_change,
+  SUM(
+    CASE WHEN c.exclusions = ' ' AND c.extras = ' ' THEN 1 
+    ELSE 0
+    END) AS no_change
+FROM pizza_runner.customer_orders AS c
+JOIN pizza_runner.runner_orders AS r
+  ON c.order_id = r.order_id
+WHERE r.distance != 0
+GROUP BY c.customer_id
+ORDER BY c.customer_id;
+
+
+-- 8. How many pizzas were delivered that had both exclusions and extras?
+
+SELECT  
+  SUM(
+    CASE WHEN exclusions IS NOT NULL AND extras IS NOT NULL THEN 1
+    ELSE 0
+    END) AS pizza_count_w_exclusions_extras
+FROM pizza_runner.customer_orders AS c
+JOIN pizza_runner.runner_orders AS r
+  ON c.order_id = r.order_id
+WHERE r.distance >= 1 
+  AND exclusions <> ' ' 
+  AND extras <> ' ';
+
+
+-- 9. What was the total volume of pizzas ordered for each hour of the day?
+
+SELECT 
+  DATEPART(HOUR, [order_time]) AS hour_of_day, 
+  COUNT(order_id) AS pizza_count
+FROM pizza_runner.customer_orders
+GROUP BY DATEPART(HOUR, [order_time]);
+
+
+-- 10. What was the volume of orders for each day of the week?
+
+SELECT 
+  FORMAT(DATEADD(DAY, 2, order_time),'dddd') AS day_of_week, 
+  COUNT(order_id) AS total_pizzas_ordered
+FROM pizza_runner.customer_orders
+GROUP BY FORMAT(DATEADD(DAY, 2, order_time),'dddd');
