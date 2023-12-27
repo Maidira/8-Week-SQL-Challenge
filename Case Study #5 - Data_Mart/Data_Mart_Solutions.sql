568bc2e82f9d0cc11d2a93340670f21d138cad2b
Avinash <60576485+Maidira@users.noreply.github.com>
2023-12-27 18:14:49 +0530

    Update Data_Mart_Solutions.sql

--- Case Study #5 - Data_Mart/Data_Mart_Solutions.sql
+++ Case Study #5 - Data_Mart/Data_Mart_Solutions.sql
@@ -176,7 +176,88 @@ ORDER BY calendar_year, platform;
 
 
 
+-----------------------------------
+-- C. Before & After Analysis --
+-----------------------------------
 
+-- 1. What is the total sales for the 4 weeks before and after 2020-06-15? What is the growth or reduction rate in actual values and percentage of sales?
+
+WITH cte AS (
+  SELECT week_date, week_number, 
+		SUM(sales) AS total_sales
+  FROM clean_weekly_sales
+  WHERE (week_number BETWEEN 21 AND 28) AND (calendar_year = 2020)
+  GROUP BY week_date, week_number
+)
+, before_after_changes AS (
+  SELECT 
+    SUM(CASE 
+      WHEN week_number BETWEEN 21 AND 24 THEN total_sales END) AS before_packaging_sales,
+    SUM(CASE 
+      WHEN week_number BETWEEN 25 AND 28 THEN total_sales END) AS after_packaging_sales
+  FROM cte
+)
+
+SELECT 
+  after_packaging_sales - before_packaging_sales AS sales_variance, 
+  ROUND(100 * 
+    (after_packaging_sales - before_packaging_sales) 
+    / before_packaging_sales,2) AS variance_percentage
+FROM before_after_changes;
+
+
+-- 2. What about the entire 12 weeks before and after?
+
+WITH cte AS (
+  SELECT week_date, week_number, 
+		SUM(sales) AS total_sales
+  FROM clean_weekly_sales
+  WHERE (week_number BETWEEN 13 AND 37) AND (calendar_year = 2020)
+  GROUP BY week_date, week_number
+)
+, before_after_changes AS (
+  SELECT 
+    SUM(CASE 
+      WHEN week_number BETWEEN 13 AND 24 THEN total_sales END) AS before_packaging_sales,
+    SUM(CASE 
+      WHEN week_number BETWEEN 25 AND 37 THEN total_sales END) AS after_packaging_sales
+  FROM cte
+)
+
+SELECT 
+  after_packaging_sales - before_packaging_sales AS sales_variance, 
+  ROUND(100 * 
+    (after_packaging_sales - before_packaging_sales) / before_packaging_sales,2) AS variance_percentage
+FROM before_after_changes;
+
+
+-- 3. How do the sale metrics for these 2 periods before and after compare with the previous years in 2018 and 2019?
+
+WITH cte AS (
+  SELECT calendar_year, week_number, 
+		SUM(sales) AS total_sales
+  FROM clean_weekly_sales
+  WHERE week_number BETWEEN 13 AND 37
+  GROUP BY calendar_year, week_number
+)
+, before_after_changes AS (
+  SELECT 
+    calendar_year,
+    SUM(CASE 
+      WHEN week_number BETWEEN 13 AND 24 THEN total_sales END) AS before_packaging_sales,
+    SUM(CASE 
+      WHEN week_number BETWEEN 25 AND 37 THEN total_sales END) AS after_packaging_sales
+  FROM cte
+  GROUP BY calendar_year
+)
+
+SELECT 
+  calendar_year, 
+  after_packaging_sales - before_packaging_sales AS sales_variance, 
+  ROUND(100 * 
+    (after_packaging_sales - before_packaging_sales) 
+    / before_packaging_sales,2) AS variance_percentage
+FROM before_after_changes;
 
 
 
